82c156ff9dc1fb2a0115f56e6b515d9fac6d2478
Ubuntu <ubuntu@ip-172-31-33-180.ec2.internal>
2026-06-17 18:07:49 +0000

    mise en niveau

--- emergency_management_project_kaarange/urls.py
+++ emergency_management_project_kaarange/urls.py
@@ -22,6 +22,11 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 
 urlpatterns = [
@@ -30,6 +35,10 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns  += staticfiles_urlpatterns()
